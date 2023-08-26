4a2ecd6575d5bd4a1141685572b02f9c3b104a87
Leon Timmermans <fawaka@gmail.com>
2023-08-26 17:19:35 +0200

    Initial code

--- lib/Magic/Check.pm
+++ lib/Magic/Check.pm
@@ -7,4 +7,26 @@ use XSLoader;
 
 XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
+use Exporter 'import';
+our @EXPORT = qw/check_variable/;
+
 1;
+
+# ABSTRACT: Add type/value checks to variables
+
+=head1 SYNOPSIS
+
+ use Magic::Check;
+ use Types::Standard 'Int';
+
+ check_variable(my $var = 1, Int);
+
+ $var = "abc"; # this will throw
+
+=head1 DESCRIPTION
+
+=func check_variable
+
+ check_variable($variable, $checker)
+
+This function takes a variable and adds set magic to check if the variable matches. This callback must be an object with a C<validate> like provided by L<Type::Tiny|Type::Tiny>: in must have a C<validate> method that returns C<undef> on success and an error message on failure. If the new value does not match, the old value is restored and the message is thrown as an exception.
